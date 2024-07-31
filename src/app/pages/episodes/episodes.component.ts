9bcc3bb0bcdabb884e39d6229bcc0fff1ec681bc
Cartaxo1 <cartaxojoao1@gmail.com>
2024-07-31 20:00:47 -0300

    add filter by Id

--- src/app/pages/episodes/episodes.component.ts
+++ src/app/pages/episodes/episodes.component.ts
@@ -1,23 +1,80 @@
 import { Component } from '@angular/core';
 import { EpisodesService } from '../../services/episodes.service';
 
+export interface ApiResponse {
+  info?: IInfo;
+  results?: IEpisodes[];
+}
+
+export interface IEpisodes {
+  id: number;
+  name: string;
+  air_date: Date;
+  episode: string;
+  characters: string;
+  url: string;
+  created: Date;
+}
+
+export interface IInfo {
+  count: number;
+  pages: number;
+  next?: string;
+  prev?: string;
+}
+
 @Component({
   selector: 'app-episodes',
   templateUrl: './episodes.component.html',
   styleUrl: './episodes.component.scss',
 })
 export class EpisodesComponent {
-  episodes: any = [];
+  episodes: ApiResponse | undefined;
+  episodesById: IEpisodes | undefined;
+  onSearch: string = '';
+  filter = [
+    { name: 'nome', value: 'name' },
+    { name: 'número', value: 'episode' },
+  ];
+  selectedFilter: any;
 
-  constructor(private episodesService: EpisodesService) {}
-
-  getEpisodes() {
-    this.episodesService.getEpisodes().subscribe((res) => {
-      this.episodes = res;
-    });
+  constructor(private episodesService: EpisodesService) {
+    this.episodes = undefined;
+    this.episodesById = undefined;
   }
 
   ngOnInit() {
     this.getEpisodes();
+  }
+
+  getEpisodes() {
+    this.episodesService.getEpisodes().subscribe((res: ApiResponse) => {
+      this.episodes = res;
+      console.log(this.episodes);
+    });
+  }
+
+  getEpisodeById() {
+    this.episodesService.getEpisodeById(this.onSearch).subscribe((res: any) => {
+      this.episodesById = res;
+      console.log(this.episodes);
+    });
+  }
+
+  getEpisodeByName() {
+    this.episodesService
+      .getEpisodeByName(this.onSearch)
+      .subscribe((res: ApiResponse) => {
+        this.episodes = res;
+        console.log(this.episodes);
+      });
+  }
+
+  searchFilter() {
+    if (this.onSearch.length <= 3) {
+      this.getEpisodeById();
+    } else if (this.onSearch.length > 3) {
+      this.getEpisodeByName();
+    }
   }
 }
--- src/app/services/episodes.service.ts
+++ src/app/services/episodes.service.ts
@@ -1,5 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {
+  ApiResponse,
+  IEpisodes,
+  IInfo,
+} from '../pages/episodes/episodes.component';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -8,7 +14,14 @@ export class EpisodesService {
   apiUrl = 'https://rickandmortyapi.com/api';
   constructor(private http: HttpClient) {}
 
-  getEpisodes() {
-    return this.http.get(`${this.apiUrl}/episode`);
+ public getEpisodes(): Observable<ApiResponse> {
+    return this.http.get<ApiResponse>(`${this.apiUrl}/episode`);
+  }
+
+  getEpisodeById(filterId: string) {
+    return this.http.get(`${this.apiUrl}/episode/${filterId}`);
+  }
+  getEpisodeByName(filter: string) {
+    return this.http.get(`${this.apiUrl}/episode/?name=${filter}`);
   }
 }
